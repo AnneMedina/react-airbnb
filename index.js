ff4b8f204c43927c731f3bd5c6ac24e32cadcb84
Anne Medina <carmelaanne.medina@gmail.com>
2024-10-10 20:20:13 +1030

    Answer to challenge 3 using template literals

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ import ReactDOM from "react-dom"
 import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
+
+
+
+
+
+
+/*
+Challenge 3:
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+
+E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
+return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+*/
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"];
+const pokewrapped = pokemon.map((pokemon) => {
+    return `<p>${pokemon}</p>`
+})
+//should return an array of strings
+console.log(pokewrapped);
